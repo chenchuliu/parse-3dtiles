266f4c773e866a913e3c4b8508d1892860bb2263
cheney hang <1623784752@qq.com>
2023-12-06 16:06:28 +0800

    完善b3dm解析

--- b3dm文件读取测试/b3dmRead.js
+++ b3dm文件读取测试/b3dmRead.js
@@ -1,13 +1,21 @@
 let fs = require("fs");
-const readStream = fs.createReadStream("./data/batchedColors.b3dm");
-
-readStream.on('data',(buffer)=>{
-
-	const b3dm = b3dmParse(buffer);
+fs.readFile('./data/tile.b3dm', (err, data)=>{
+	if(err){
+		console.error('Error reading file:', err);
+    return;
+	}
+	const b3dm = b3dmParse(data);
 
 	gltfParse(b3dm.gltf);
-
 })
+// fs.readFile('./data/untitled.glb', (err, data)=>{
+// 	if(err){
+// 		console.error('Error reading file:', err);
+//     return;
+// 	}
+// 	const gltf = new Uint8Array(data.buffer, 0);
+// 	gltfParse(gltf);
+// })
 
 // 解析 .b3dm 文件
 function b3dmParse(buffer){
@@ -85,10 +93,10 @@ function gltfParse(glb){
 		throw new Error('data is not glTF')
 	}
 	uint8Array = glb.subarray(offset, offset += 4);
-	const version = uint8ToUint32(uint8Array);
+	const gltfVersion = uint8ToUint32(uint8Array);
 
 	uint8Array = glb.subarray(offset, offset += 4);
-	const length = uint8ToUint32(uint8Array);
+	const gltfLength = uint8ToUint32(uint8Array);
 
 	uint8Array = glb.subarray(offset, offset += 4);
 	const chunk1Length = uint8ToUint32(uint8Array);
@@ -98,8 +106,9 @@ function gltfParse(glb){
 	let chunk1Data;
 	if(chunk1Type === 'JSON'){
 		uint8Array = glb.subarray(offset, offset += chunk1Length);
-		console.log(new TextDecoder('utf-8').decode(uint8Array));
+		console.log(new TextDecoder('utf-8').decode(uint8Array))
 		chunk1Data = JSON.parse(new TextDecoder('utf-8').decode(uint8Array));
+		
 	}else{
 		
 	}
@@ -113,13 +122,14 @@ function gltfParse(glb){
 		uint8Array = glb.subarray(offset, offset += chunk1Length);
 		chunk2Data = JSON.parse(new TextDecoder('utf-8').decode(uint8Array));
 	}else{
-		uint8Array = glb.subarray(offset,offset += chunk2Length)
-		chunk2Data = readBINData(chunk1Data, uint8Array);
+		// uint8Array = glb.subarray(offset,offset += chunk2Length);
+		const binView = new DataView(uint8Array.buffer, glb.byteOffset + offset, chunk2Length);
+		chunk2Data = readBINData(chunk1Data, binView);
 	}
 
 }
 
-function readBINData(chunk1Data, uint8Array){
+function readBINData(chunk1Data, dataview){
 	let offset = 0;
 	const accessors = chunk1Data.accessors;
 	const bufferViews = chunk1Data.bufferViews;
@@ -127,23 +137,54 @@ function readBINData(chunk1Data, uint8Array){
 	for (let i = 0; i < accessors.length; i++) {
 		const accessor = accessors[i];
 		if(accessor.componentType === 5126 && accessor.type === 'VEC3'){
-			// FLOAT（Float32Array） VEC3 表示三维矢量（一般是顶点坐标
-			const maxOffset = bufferViews[accessor.bufferView].byteOffset + accessor.count * 3 * 4;
-			while(offset <= maxOffset){
-				const x = new Float32Array(uint8Array.subarray(offset, offset += 4));
-				const y = new Float32Array(uint8Array.subarray(offset, offset += 4));
-				const z = new Float32Arrayuint8Array.subarray(offset, offset += 4);
+			// FLOAT（Float32Array） VEC3 表示三维矢量（一般是顶点坐标, uv坐标, 颜色值, 法线等)
+			// const maxOffset = bufferViews[accessor.bufferView].byteOffset + accessor.byteOffset + accessor.count * 3 * 4;
+			const maxOffset = offset + accessor.count * 3 * 4;
+			while(offset < maxOffset){
+				const x = dataview.getFloat32(offset, true);
+				offset += 4;
+				const y = dataview.getFloat32(offset, true);
+				offset += 4;
+				const z = dataview.getFloat32(offset, true);
+				offset += 4;
 				positions.push([x,y,z]);
+				console.log(`Float32 VEC3 x:${x},y:${y},z:${z}`);
 			}
-			
-		} else if(accessor.componentType === 5126 && accessor.type === 'VEC3'){
-
+		}else if(accessor.componentType === 5126 && accessor.type === 'VEC2'){ 
+			// UNSIGNED_SHORT（Uint16Array） SCALAR 表示标量无符号短值（一般是索引）
+			// const maxOffset = bufferViews[accessor.bufferView].byteOffset + accessor.byteOffset + accessor.count * 2;
+			const maxOffset = offset + accessor.count * 2 * 4;
+			while(offset < maxOffset){
+				const x = dataview.getFloat32(offset, true);
+				offset += 4;
+				const y = dataview.getFloat32(offset, true);
+				offset += 4;
+				console.log(`Float32 VEC2 x:${x},y:${y}`);
+			}
+		} else if(accessor.componentType === 5126 && accessor.type === 'SCALAR'){
+			// UNSIGNED_SHORT（Uint16Array） SCALAR 
+			// const maxOffset = bufferViews[accessor.bufferView].byteOffset + accessor.byteOffset + accessor.count * 4;
+			const maxOffset = offset + accessor.count * 4;
+			while(offset < maxOffset){
+				const x = dataview.getFloat32(offset, true);
+				offset += 4;
+				console.log(`Float32 SCLALAR:${x}`);
+			}
 		} else if(accessor.componentType === 5123 && accessor.type === 'SCALAR'){ 
 			// UNSIGNED_SHORT（Uint16Array） SCALAR 表示标量无符号短值（一般是索引）
-		}
-		// accessor.type 还有MAT4，这里不列举
+			// const maxOffset = bufferViews[accessor.bufferView].byteOffset + accessor.byteOffset + accessor.count * 2;
+			const maxOffset = offset + accessor.count * 2;
+			while(offset < maxOffset){
+				const x = dataview.getUint16(offset, true);
+				offset += 2;
+				console.log(`Uint16 SCLALAR x:${x}`);
+			}
+		} 
+		
+		// accessor.type 还有MAT4， VEC2，这里不列举
 
 	}
+	console.log(offset)
 }
 
 function uint8ToUint32(uint8Array){
